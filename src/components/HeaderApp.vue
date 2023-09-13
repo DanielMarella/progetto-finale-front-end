50dcc480a60db152912cc92e2a40e77a55a3bcad
DanielMarella <marella.daniel96@gmail.com>
2023-09-13 10:31:37 +0200

    fix

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -36,18 +36,17 @@ export default {
                     name: 'SingleMusician'
                 },
                 {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     route: './Register',
                     name: 'Register'
-=======
+
                     route: './Login',
                     name: 'Login'
->>>>>>> 05d3d2e1e1ddd1167ebe544668aafd4d4e36133e
-=======
+
+
                     route: './Login',
                     name: 'Login'
->>>>>>> 05d3d2e1e1ddd1167ebe544668aafd4d4e36133e
+
                 },
             ]
         }
