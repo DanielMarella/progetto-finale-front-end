dc41f7772a26c8206e5378b6137d0a132272017e
Al-Giovinetti <giovinettialessio@gmail.com>
2023-09-26 19:06:50 +0200

    modified SingleMusician style

--- src/pages/SingleMusician.vue
+++ src/pages/SingleMusician.vue
@@ -1,94 +1,91 @@
 <template>
 <div class="container">
+    <!-- Scheda tecnica musicista -->
     <div class="row">
-        <div class="card col-10 mx-auto my-4">
-            <div class="card-body">
-                <div class="d-flex justify-content-between">
-                    <div>
-                        <h5 class="card-title">
-                            {{ user.name }} {{ musicians.surname }}
-                        </h5>
-                        
-                        <p class="card-text">
-                            {{ musicians.address }}
-                        </p>
-                        
+        <div class="card col-10 mx-auto p-0 my-4">
+            <div class="card-header">
+                <h3>Schede Musicista</h3>
+            </div>
+            <div class="card-body row py-0">
+                <!-- Scheda di sx-->
+                <div class="musicianInfo col-6 d-flex flex-column align-items-center justify-content-around">
+                    <h3 class="card-title">
+                        {{ user.name }} {{ musicians.surname }}
+                    </h3>
 
-                    </div>
-                    
                     <div class="imageWrapper">
-                        <!--Qui inseriremo la foto del musicista-->
                         <img v-if="musicians.image.startsWith('http')" :src="musicians.image" :alt="user.name + musicians.surname + ' image'">
                         <img v-else :src=" 'http://127.0.0.1:8000/storage/' + musicians.image" :alt="user.name + musicians.surname + ' image'">
                     </div>
+                    
+                    <p class="card-text fs-5">
+                        {{ musicians.address }}
+                    </p>
+                </div>
+
+                <!-- Scheda di dx -->
+                <div class="musician-skill col-6 border-start">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item d-flex gap-3">
+                            <span>
+                                Strumenti:
+                            </span>
+
+                            <p v-for="instrument in musicalInstrument" class="m-0">
+                                {{ instrument.name }}
+                            </p>
+                        </li>
+                        
+                        <li class="list-group-item">
+                            Generi: {{ musicians.musical_genre }}
+                        </li>
+
+                        <li class="list-group-item">
+                            Prezzo: &euro;{{ musicians.price }}
+                        </li>
+
+                        <li class="list-group-item">
+                            <span>Curriculm Vitae</span>
+                            <div class="cvWrapper">
+                                <img v-if="musicians.cv.startsWith('http')" :src="musicians.cv" :alt="user.name + musicians.surname + ' cv'">
+                                <img v-else :src=" 'http://127.0.0.1:8000/storage/' + musicians.cv" :alt="user.name + musicians.surname + ' cv'">
+                            </div>   
+                        </li>
+                    </ul>
                 </div>
             </div>
 
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item d-flex gap-3">
-                    <span>
-                        Strumenti:
-                    </span>
-
-                    <div v-for="instrument in musicalInstrument">
-                        <p>
-                            {{ instrument.name }}
-                        </p>
-                    </div>
-                </li>
-                
-                <li class="list-group-item">
-                    Generi: {{ musicians.musical_genre }}
-                </li>
-
-                <li class="list-group-item">
-                    Prezzo: &euro;{{ musicians.price }}
-                </li>
-
-                <li class="list-group-item">
-                    Curriculum Vitale:
-                    <div class="cvWrapper">
-                        <img v-if="musicians.cv.startsWith('http')" :src="musicians.cv" :alt="user.name + musicians.surname + ' cv'">
-                    <img v-else :src=" 'http://127.0.0.1:8000/storage/' + musicians.cv" :alt="user.name + musicians.surname + ' cv'">
-                    </div>
-                    
-                </li>
-            </ul>
-
-            <div class="card-body">
-                <a href="#" class="card-link">
-                    Sito Web
-                </a>
-
-                <a href="#" class="card-link">
-                    Link Social
-                </a>
+            <!-- Form Recensione --> 
+            <div class="card-footer text-center">
+                <button type="button" class="btn btn-primary" @click="becomesActiveReview()">Lascia una recensione</button>
+                <ReviewForm :class="activeReview"/>
             </div>
         </div>
     </div>
 
-
-    <ReviewForm />
-
-
+    <!-- Form Messaggio -->
     <div class="row">
-        <div class="card col-10 mx-auto my-4">
-            <h2>
-                Contatti
-            </h2>
+        <div class="card col-10 mx-auto my-4 p-0">
+            <div class="card-header">
+                <h3>Contatti</h3>
+            </div>
+            <div class="card-body">
+                <div class="d-flex justify-content-evenly">
+                    <p>
+                        Cellulare: {{ musicians.num_phone }}
+                    </p>
 
-            <p>
-                Cellulare: {{ musicians.num_phone }}
-            </p>
-
-            <p>
-                EMAIL : {{ user.email }}
-            </p>
-
-        <ContactForm />
-    
+                    <p>
+                        EMAIL : {{ user.email }}
+                    </p>
+                </div>
+                <div class="text-center">
+                    <button type="button" class="btn btn-primary " @click="becomesActiveMex()">Contatta il musicista</button>
+                </div>
+                <ContactForm :class="activeMex"/>
+            </div>    
         </div>
-    </div>
+    </div>   
 </div>
 
 </template>
@@ -116,6 +113,10 @@ export default {
             apiReview: 'http://127.0.0.1:8000/api/review-form',
             musicians: [],
             user: [],
+
+            activeReview: 'd-none',
+            activeMex: 'd-none'
+
         };
     },
     methods: {
@@ -128,6 +129,22 @@ export default {
                 console.log(this.user);
             });
         },
+
+        becomesActiveReview(){
+            if(this.activeReview == 'd-none'){
+                this.activeReview='d-block';
+            }else{
+                this.activeReview= 'd-none';
+            }
+        },
+
+        becomesActiveMex(){
+            if(this.activeMex == 'd-none'){
+                this.activeMex='d-block';
+            }else{
+                this.activeMex= 'd-none';
+            }
+        }
 
        
     },
@@ -143,8 +160,8 @@ export default {
 <style lang="scss">
     div.imageWrapper{
         img{
-            width: 90px;
-            height: 90px;
+            width: 250px;
+            height: 250px;
             border-radius: 50%;
             object-fit: cover;
         }
@@ -155,4 +172,5 @@ export default {
             width: 100%;
         }
     }
+
 </style>
