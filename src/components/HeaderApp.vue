430f385e2998b23c6f2ea9c50b2eaa55d192af02
DanielMarella <marella.daniel96@gmail.com>
2023-09-28 09:25:35 +0200

    fix nav

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,11 +1,11 @@
 <template>
 
     <!-- navbar -->
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
 
         <!-- logo -->
-        <a class="navbar-brand text-dark logo" href="#"><span class="specialText">BOO</span>SICIAN</a>
+        <a class="navbar-brand text-dark" href="#">Nome Sito</a>
 
         <!-- Toggle btn -->
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
@@ -25,8 +25,8 @@
             <div class="offcanvas-body d-flex flex-column flex-lg-row p-4 p-lg-0">
                 <ul class="navbar-nav justify-content-center align-items-center flex-grow-1 pe-3">
                     <li class="nav-item mx-2 my-3" v-for="link in links">
-                        <router-link :to="{name:link.name}" class="text-black text-decoration-none px-3">
-                        {{ link.label }}
+                        <router-link :to="link.route" class="text-black text-decoration-none px-3">
+                        {{ link.name }}
                         </router-link>
                     </li>
                 </ul>
@@ -35,8 +35,8 @@
                 <div class="d-flex justify-content-center  align-items-center">
                     <ul class="d-flex flex-column  flex-lg-row m-0 gap-3 p-0">
                         <li class="list-unstyled">
-                            <button class="btn btn-small btnSignIn" ><a href="http://127.0.0.1:8000/" class="firstBtnText fw-700">Accedi</a></button>
-                            <button @click="redirectToLaravelLogin" class="btn btn-small btnLogIn"><a href="http://127.0.0.1:8000/register" class="fw-700">Registrati</a></button>
+                            <button class="btn btn-small btn-primary" ><a href="http://127.0.0.1:8000/">Login</a></button>
+                            <button @click="redirectToLaravelLogin" class="btn btn-small btn-danger"><a href="http://127.0.0.1:8000/register">Register</a></button>
                         </li>
                     </ul>
                 </div>
@@ -44,6 +44,19 @@
         </div>
     </div>
 </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 </template>
@@ -60,11 +73,11 @@ export default {
             links : [
                 {
                     route: './',
-                    name: 'Home'
+                    name: 'HomePage'
                 },
                 {
                     route: './advance-search',
-                    name: 'Ricerca'
+                    name: 'AdvanceSearch'
                 },
             ],
             
@@ -82,8 +95,6 @@ export default {
 
 
 <style lang="scss" scoped>
-@use '../styles/partials/_variables.scss' as variables;
-
     @media (max-width: 991px) {
         .sidebar{
             background-color: rgba(255, 255, 255, .15);
@@ -95,31 +106,4 @@ export default {
         text-decoration: none;
         color: white;
     }
-
-    nav{
-        background-color: variables.$primaryColor;
-
-        a.logo{
-            font-weight: 900;
-            font-size: 2.5rem;
-        }
-    }
-
-    .specialText{
-        color: variables.$secondaryColor;
-    }
-
-    .firstBtnText{
-        color: variables.$primaryColor;
-    }
-
-        button.btn.btnSignIn{
-            background-color: variables.$secondaryColor;
-            border-color: variables.$secondaryColor;
-            margin-right: 1rem;
-        }
-        button.btn.btnLogIn{
-            background-color: variables.$accentColor;
-            border-color: variables.$accentColor;
-        }
 </style>
